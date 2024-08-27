91be9b8fd3f3fbff13c35d8e921ff3fc2bd74ff5
Fire <17692105+FireMario211@users.noreply.github.com>
2024-08-27 16:28:21 -0400

    fix some setup stuff

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -10,19 +10,21 @@ rm geode-cli-v3.2.0-linux.zip
 
 wget https://github.com/Jake-Shadle/xwin/releases/download/0.6.5/xwin-0.6.5-x86_64-unknown-linux-musl.tar.gz
 tar -xzf xwin-0.6.5-x86_64-unknown-linux-musl.tar.gz
-sudo mv xwin /usr/local/bin/
+sudo mv xwin-0.6.5-x86_64-unknown-linux-musl/xwin /usr/local/bin/
+rm -rf xwin-0.6.5-x86_64-unknown-linux-musl
 rm xwin-0.6.5-x86_64-unknown-linux-musl.tar.gz
 
+mkdir -p ~/xwin
 mkdir -p ~/xwin/splat
 cd ~/xwin
 xwin --arch x86_64 --accept-license splat --include-debug-libs --output splat
 
 git clone https://github.com/matcool/clang-msvc-sdk.git toolchain
 
+sudo apt-get clean
+sudo rm -rf /var/lib/apt/lists/*
+
 mkdir ~/geode
 export GEODE_SDK = ~/geode
 geode install ~/geode
 geode install-binaries
-
-sudo apt-get clean
-sudo rm -rf /var/lib/apt/lists/*
