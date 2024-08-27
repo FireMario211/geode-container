0af1bd6f1af6bb850d3427965f7952031181cecf
Fire <17692105+FireMario211@users.noreply.github.com>
2024-08-27 16:53:51 -0400

    fix some outdated things

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,30 +1,38 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y clang-17 clang-tools-17 lld-17 wget unzip
+sudo apt-get install wget software-properties-common gnupg
+# i shouldnt NEED to do this but the latest ver on ubuntu is clang-14!?
+wget https://apt.llvm.org/llvm.sh
+chmod +x llvm.sh
+sudo ./llvm.sh 17
+sudo apt-get update
+sudo apt-get install unzip pip ninja-build
+sudo apt install git clang-17 clang-tools-17 lld-17
+pip install cmake --upgrade # because for whatever reason it installs an outdated version, ig source repos arent up to date!
 
 wget https://github.com/geode-sdk/cli/releases/download/v3.2.0/geode-cli-v3.2.0-linux.zip
 unzip geode-cli-v3.2.0-linux.zip
+chmod +x geode
 sudo mv geode /usr/local/bin/
 rm geode-cli-v3.2.0-linux.zip
 
 wget https://github.com/Jake-Shadle/xwin/releases/download/0.6.5/xwin-0.6.5-x86_64-unknown-linux-musl.tar.gz
 tar -xzf xwin-0.6.5-x86_64-unknown-linux-musl.tar.gz
+chmod +x xwin-0.6.5-x86_64-unknown-linux-musl/xwin
 sudo mv xwin-0.6.5-x86_64-unknown-linux-musl/xwin /usr/local/bin/
 rm -rf xwin-0.6.5-x86_64-unknown-linux-musl
 rm xwin-0.6.5-x86_64-unknown-linux-musl.tar.gz
 
-mkdir -p ~/xwin
 mkdir -p ~/xwin/splat
 cd ~/xwin
 xwin --arch x86_64 --accept-license splat --include-debug-libs --output splat
 
 git clone https://github.com/matcool/clang-msvc-sdk.git toolchain
 
+geode sdk install ~/geode
+export GEODE_SDK=~/geode
+geode sdk install-binaries
+
 sudo apt-get clean
 sudo rm -rf /var/lib/apt/lists/*
-
-mkdir ~/geode
-export GEODE_SDK = ~/geode
-geode install ~/geode
-geode install-binaries
